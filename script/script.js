4a3ff3a58041e0ae8a6bb7836cea52cc137a661a
Sávio <saviodeveloper@proton.me>
2023-05-17 22:51:25 -0300

    Barra De Progresso Horizontal
    Finalizada e funcional.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-const 
+window.onscroll = function() {
+    var posicao = document.documentElement.scrollTop
+    var posicaoRelativa = document.documentElement.scrollHeight-
+                          document.documentElement.clientHeight
+    var scroll = posicao * 100 / posicaoRelativa
+
+    document.getElementById("BarraProgressoHorizontal").style.width = scroll + "%"
+}  
