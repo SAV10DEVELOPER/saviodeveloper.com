f41f136712c69415715f18ff1d6d01212513b1d2
Sávio <40009425+SAV10DEVELOPER@users.noreply.github.com>
2023-07-31 16:25:20 -0300

    • Update: Barra de progressão e botão to top
    // Correção
    + Barra de progressão horizontal
    + Botão scroll to top

--- script/script.js
+++ script/script.js
@@ -1,27 +1,27 @@
-// Barra de progressão horizontal no topo da página
-window.onscroll = function() {
-    var posicao = document.documentElement.scrollTop
-    var posicaoRelativa = document.documentElement.scrollHeight-
-                          document.documentElement.clientHeight
-    var scroll = posicao * 100 / posicaoRelativa
+// Barra de progressão horizontal
+window.onscroll = function () {
+  var posicao = document.documentElement.scrollTop
+  var posicaoRelativa = document.documentElement.scrollHeight -
+                        document.documentElement.clientHeight
+  var scroll = (posicao * 100) / posicaoRelativa
 
-    document.getElementById("BarraProgressoHorizontal").style.width = scroll + "%"
-}  
+  document.getElementById("BarraProgressoHorizontal").style.width = scroll + "%"
 
-// Botão de scroll top
-const scrollTop = document.getElementById('ScrollTop')
-
-window.onload = () => { 
-    scrollTop.style.visibility = 'hidden'
-    scrollTop.style.opacity = 0
-}
-
-window.onscroll = () => {
+  // Botão de "scroll top"
+  const scrollTop = document.getElementById("ScrollTop")
     if (window.scrollY > 200) {
-        scrollTop.style.visibility = 'visible'
-        scrollTop.style.opacity = 1
-    } else {
-        scrollTop.style.visibility = 'hidden'
-        scrollTop.style.opacity = 0
-    }
-}
+      scrollTop.style.visibility = "visible"
+      scrollTop.style.opacity = 1 } 
+    else {
+      scrollTop.style.visibility = "hidden"
+      scrollTop.style.opacity = 0 } }
+
+// Função para ir para o topo da página
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" }) }
+
+// Mostrar o botão de "scroll top" ao carregar a página
+  document.addEventListener("DOMContentLoaded", function () {
+    const scrollTop = document.getElementById("ScrollTop")
+    scrollTop.style.visibility = "hidden"
+    scrollTop.style.opacity = 0 })
