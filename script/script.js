ed825ecab02b5734e992263f99bba8fd8a94d5d3
Sávio <40009425+SAV10DEVELOPER@users.noreply.github.com>
2023-08-02 20:45:18 -0300

    ••• Slide Mid Page
    • Finalizado

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// Barra de progressão horizontal
+// BARRA DE PROGRESSÃO HORIZONTAL: Barra no topo da página para ir medindo a progressão vertical
 window.onscroll = function () {
   var posicao = document.documentElement.scrollTop
   var posicaoRelativa = document.documentElement.scrollHeight -
@@ -7,31 +7,38 @@ window.onscroll = function () {
 
   document.getElementById("BarraProgressoHorizontal").style.width = scroll + "%"
 
-  // Botão de "scroll top"
+// BOTÃO SCROLL TO TOP: Batão para voltar para o topo da página
   const scrollTop = document.getElementById("ScrollTop")
-    if (window.scrollY > 200) {
-      scrollTop.style.visibility = "visible"
-      scrollTop.style.opacity = 1 } 
-    else {
-      scrollTop.style.visibility = "hidden"
-      scrollTop.style.opacity = 0 } }
-
-// Função para ir para o topo da página
-  function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: "smooth" }) }
-
-// Mostrar o botão de "scroll top" ao carregar a página
-  document.addEventListener("DOMContentLoaded", function () {
-    const scrollTop = document.getElementById("ScrollTop")
+  if (window.scrollY > 200) {
+    scrollTop.style.visibility = "visible"
+    scrollTop.style.opacity = 1 }   
+  else {
     scrollTop.style.visibility = "hidden"
-    scrollTop.style.opacity = 0 })
+    scrollTop.style.opacity = 0 } }
 
-// Slide mid page
-  var splide = new Splide( '.splide', {
-      type   : 'loop',
-      drag   : 'free',
-      perPage: 5,
-      focus  : 'center',
-  } );
-    
-splide.mount();
+  function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" }) }
+
+  document.addEventListener("DOMContentLoaded", function () {
+  const scrollTop = document.getElementById("ScrollTop")
+  scrollTop.style.visibility = "hidden"
+  scrollTop.style.opacity = 0 })
+
+// SLIDE MID PAGE: Com conteúdos 3 conteúdos de destaque de cada tema
+var splide = new Splide( '.splide', {
+  type   : 'loop',
+  drag   : 'free',
+  perPage: 5,
+  perMove: 3,
+  gap: '2rem',
+  snap: true,
+
+/* CONFIGURAÇÕES EXTRAS:
+  padding: '3rem', 
+  arrows: true,
+  pagination: true,
+  autoplay: true,
+*/
+} )
+
+splide.mount()
